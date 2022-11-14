f41a5d6784178fe28676ebab3e281be2e4d0b3e7
Michaela Křenková <misakren@gmail.com>
2022-11-14 22:40:54 +0100

    Finální verze

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('funguju!');
+const timer = Number(prompt("Jaký chcete nastavit odpočet v sekundách? ")) * 1000
+
+const alarmElm = document.querySelector('.alarm')
+const audioElm = document.querySelector('audio')
+const buttonElm = document.querySelector('.buttonStop')
+
+const timeIsUp = () => {
+	alarmElm.classList.add('alarm--ring')
+	audioElm.play()
+}
+
+const timerId = setTimeout(timeIsUp, timer)
+
+buttonElm.addEventListener('click', () => {
+	clearTimeout(timerId)
+	buttonElm.textContent = 'Zastaveno'
+})
